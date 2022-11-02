d51aaf85823b045ff79151e1e0ccef5e24e79ad5
TartCodes <tartcodes@gmail.com>
2022-11-02 16:38:22 -0400

    comps

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
 
+import './ExpenseItem.css'
 
 const ExpenseItem = () => {
   return (
-    <div>ExpenseItem</div>
+    <div className="expense-item">
+        <div>March 28th 2021</div>
+        <div className="expense-item_description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">$249</div>
+        </div>
+    </div>
   )
 }
 
