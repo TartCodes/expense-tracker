fa5ec13f3c4a9798bd505e014303d0c10d81dc1d
TartCodes <tartcodes@gmail.com>
2022-11-03 12:20:06 -0400

    new comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 
 import './ExpenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = (props) => {
-    const month = props.date.toLocaleString('en-US', { month: 'long'})
-    const day = props.date.toLocaleString('en-US', { date: '2-digit'})
-    const year = props.date.getFullYear()
+   
 
   return (
-    <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-        </div>
+    <div className="expense-item"> 
+    <ExpenseDate date={props.date}/>       
         <div className="expense-item_description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
